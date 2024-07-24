784e988f8e9f5b23d2c2b1e86eb18a48b9aa2715
michellemarcondes <131290236+michellemarcondes@users.noreply.github.com>
2024-07-24 20:23:20 -0300

    Update vr2207.1.py

--- vr2207.1.py
+++ vr2207.1.py
@@ -3,21 +3,9 @@ import hashlib
 #funcionario--Atributos: Nome; Cargo; Salario; Lotação.--Ações: Consultar salario; Consultar lotação e solicitar aumento
 class funcionario:
         def __init__(self, nome, cargo, salario, lotacao):
-        self.__nome = nome
-        self.__cargo = cargo
-        self.__salario = salario
-        self.__lotacao = lotacao
-
-        def set_nome(self, nome):#Define o nome
             self.__nome = nome
-
-        def set_nome(self, cargo):#Define o cargo
             self.__cargo = cargo
-
-        def set_nome(self, salario):#Define o salario
             self.__salario = salario
-        
-        def set_nome(self, lotacao):#Define a lotação
             self.__lotacao = lotacao
 
         def get_salario(self):#retorna o salario quando solicitado
@@ -30,3 +18,27 @@ class funcionario:
              #escrever aqui dps
         
 #Classe gerente
+class gerente(funcionario):
+     def __init__(self, nome, cargo, salario, lotacao):
+          super().__init__(self, nome, cargo, salario*2, lotacao)
+          self.funcionarios = []
+     
+     def contratar(self, funcionario):
+          self.funcionarios.append(funcionario)
+          print(f"{funcionario.nome} foi contratado(a) como {funcionario.cargo}.")
+
+    def demitir(self, funcionario):
+     if funcionario in self.funcionarios:
+          print(f"{funcionario.nome} foi demitido(a) do cargo de {funcionario.cargo}.")
+        else:
+          print(f"{funcionario.nome} não esta na lista de funcionarios.")
+
+
+funcionario1 = funcionario("João", "Analista", 3000, "financeiro")
+funcionario2 = funcionario("ana", "assistente", 3000, "Recursos humanos")
+gerente1 = gerente ("Maria", "gerente", 3000, "financeiro")
+
+gerente.contratar(funcionario1)
+gerente.contratar(funcionario2)
+print(gerente.funcionarios)
+
